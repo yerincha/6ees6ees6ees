227ad2852aa25c5fbb1ab0f42a68ff225db06537
Yerin Cha <lenacha32@gmail.com>
2019-04-15 13:02:27 -0700

    Completed 6ees.

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,14 @@
-class HoneyMakerBee {
-  // TODO..
+class HoneyMakerBee extends Bee {
+  constructor(){
+    super()
+    this.age = 10;
+    this.job = 'make honey'
+    this.honeyPot = 0;
+    this.makeHoney = function () {
+      this.honeyPot++
+    }
+    this.giveHoney = function () {
+      this.honeyPot--
+    }
+  }
 };
